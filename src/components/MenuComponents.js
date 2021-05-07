21a15af2c7ff9dba0f0c158198ae3c2a40d6edbe
Chris Peter <peterchris@gmail.com>
2021-05-07 18:06:10 +0530

    single page 2

--- src/components/MenuComponents.js
+++ src/components/MenuComponents.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { Card,CardImg,CardImgOverlay,CardText,CardBody,CardTitle } from 'reactstrap';
-
+import { Card,CardImg,CardImgOverlay,CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
     function RenderMenuItems({ dish, onClick }){
         return(
-            <Card onClick={() => onClick(dish.id)}>
-                        
-                <CardImg width="100%" object src={dish.image} alt={dish.name} />
-                
-                <CardImgOverlay>
-                    <CardTitle>{dish.name}</CardTitle>
+            <Card>
+                  <Link to={`/menu/${dish.id}`} >    
+                    <CardImg width="100%" object src={dish.image} alt={dish.name} />
                     
-                </CardImgOverlay>
+                    <CardImgOverlay>
+                        <CardTitle>{dish.name}</CardTitle>
+                        
+                    </CardImgOverlay>
+                </Link>  
             </Card>
 
         );
@@ -23,7 +24,7 @@ import { Card,CardImg,CardImgOverlay,CardText,CardBody,CardTitle } from 'reactst
         const menu = props.dishes.map((dish) => {
             return(
                 <div key={dish.id} className="col-12 col-md-5 m-1">
-                   <RenderMenuItems dish={dish} onClick={props.onClick} />
+                   <RenderMenuItems dish={dish} />
                 </div>
                
             )
@@ -31,6 +32,17 @@ import { Card,CardImg,CardImgOverlay,CardText,CardBody,CardTitle } from 'reactst
 
         return(
             <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Menu</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>Menu</h3>
+                        <hr />
+                    </div>
+                </div>
+
                 <div className="row">
                     
                         {menu}

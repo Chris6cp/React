ceb0626079f1ec8ca9ce772432bccf78765d3ab5
Chris Peter <peterchris@gmail.com>
2021-05-14 13:40:22 +0530

    redux thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Card,CardImg,CardText,CardBody,CardTitle,BreadcrumbItem,Breadcrumb, Button, ModalHeader, Modal, ModalBody, Row, Col, Label, Input } from 'reactstrap';
 import { Link, withRouter } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import {Loading } from './LoadingComponent';
 
 
    
@@ -59,9 +60,26 @@ function RenderDish({dish}){
 
     const Dishdetail = (props) => 
     {
-
-    
-        if (props.dish == null) {
+        if (props.isLoading) {
+            return (
+                <div className="container">
+                    <div className="row">
+                        <Loading />
+                    </div>
+                </div>
+            );
+        }
+        else if (props.errMess){
+            return (
+                <div className="container">
+                    <div className="row">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            );
+        }
+        
+        else if (props.dish == null) {
             return (
             <div></div>
             );

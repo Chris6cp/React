59086fee75205496189874f4556e7cec014dc021
Chris Peter <peterchris@gmail.com>
2021-05-15 12:29:18 +0530

    redux thunk

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,7 +3,7 @@ import Header from './HeaderComponent';
 import Home from './HomeComponent';
 import Menu from './MenuComponents';
 import DishDetail from './DishdetailComponent';
-
+import { actions } from 'react-redux-form';
 import Footer from './FooterComponent';
 import { Switch, Route , Redirect, withRouter } from 'react-router-dom';
 import Contact from './ContactComponent';
@@ -23,7 +23,8 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => ({
   
   addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
-  fetchDishes: () =>{dispatch(fetchDishes())}
+  fetchDishes: () =>{dispatch(fetchDishes())},
+  resetFeedbackForm: () => {dispatch(actions.reset('feedback'))}
 
 });
 
@@ -70,7 +71,7 @@ class Main extends Component {
           <Route path="/home" component={HomePage} />
           <Route exact path="/menu" component={() => <Menu dishes={this.props.dishes} />} />
           <Route path="/menu/:dishId" component={DishWithId} />
-          <Route exact path="/contactus" component ={Contact} />
+          <Route exact path="/contactus" component ={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
           <Route path="/aboutus" component ={() => <About leaders={this.props.leaders} />} />
           <Redirect to="/home" />
         </Switch>

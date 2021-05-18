12cc8487503386c5b73abcd18831bb7753630f67
Chris Peter <peterchris@gmail.com>
2021-05-18 17:46:33 +0530

    build and deploy

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card,CardImg,CardText,CardBody,CardTitle,BreadcrumbItem,Breadcrumb, Button, ModalHeader, Modal, ModalBody, Row, Col, Label, Input } from 'reactstrap';
+import { Card,CardImg,CardText,CardBody,CardTitle,BreadcrumbItem,Breadcrumb, Button, ModalHeader, Modal, ModalBody, Row, Col, Label } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import {Loading } from './LoadingComponent';
